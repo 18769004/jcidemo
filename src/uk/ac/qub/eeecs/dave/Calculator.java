8a427156213206ab9e998b205a146bbf1c72a6cb
Michelle Oakes <moakes02@qub.ac.uk>
2021-08-29 20:32:39 +0100

    Update

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -1,13 +1,20 @@
 package uk.ac.qub.eeecs.dave;
 
-public class Calculator {
+
+public class Calculator{
+	
+	
 	public int add(int a, int b) {
 		return a+b;
 	}
-	
 	public int sub(int a, int b) {
 		return a-b;
+		
 	}
-	
+	public int div(int c, int d) {
+		return c/d;
+	}
+	public int mul(int c, int d) {
+		return c*d;
+	}
 }
-
